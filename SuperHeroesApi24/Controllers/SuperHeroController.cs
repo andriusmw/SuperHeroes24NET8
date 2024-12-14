0cdd5e17d08f60887266d11a7cef7834f89cb9cc
andriusmw <andriusmw@yahoo.es>
2024-12-14 15:23:37 +0100

    PUT Working

--- SuperHeroesApi24/Controllers/SuperHeroController.cs
+++ SuperHeroesApi24/Controllers/SuperHeroController.cs
@@ -51,6 +51,27 @@ namespace SuperHeroesApi24.Controllers
 
 
 
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero updatedHero)
+        {
+            var dbHero = await _context.SuperHeroes.FindAsync(updatedHero.Id);
+            if (dbHero is null)
+            {
+                return NotFound("Hero not found.");
+            }
+
+            dbHero.Name = updatedHero.Name;
+            dbHero.FirstName = updatedHero.FirstName;
+            dbHero.LastName = updatedHero.LastName;
+            dbHero.Place = updatedHero.Place;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+
+
+
 
 
 
